22de6e3e49298e6d9f5d5161b13c68de55616995
George Favour C <phavorsparks@gmail.com>
2022-01-14 18:02:15 +0100

    clean up CRA template with dependency updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material"
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <Box>Hello world</Box>
 }
 
-export default App;
+export default App
